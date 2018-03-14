3fc57fdff79e7991bb60731b8007b7d882c1f09f
LAPTOP-MMUQEE88\Tinku <pragya.ieee@gmail.com>
2018-03-14 11:46:02 +0200

    Implemented Internationalization(I18n)

--- src/components/header.js
+++ src/components/header.js
@@ -1,60 +1,81 @@
 import React from "react";
 import { NavLink, withRouter } from "react-router-dom";
+import { I18n } from "react-i18next";
+import { translate } from "react-i18next";
 import IBM from "../images/IBM.png";
 import styles from "../styles/components/header.scss";
 
-console.log("styles: ",styles);
+const Header = (props) => {
 
-const Header = () => {
-    return (        
-        <div className={styles.menu}>
-            <div>
-                <img src={IBM} alt="IBM" />
-            </div>
-            <ul className={styles["flex-container"]} >
-                <li className={styles["flex-item"]}>
-                    <NavLink exact to="/home/info-form"
-                        isActive={(match, location) => {              
-                            return location.pathname === "/home" ||
-                location.pathname === "/home/info-form";
-                        }
-                        }
-                        activeStyle={{
-                            fontWeight: "bold",
-                            color: "#047cc0"
-                        }}>
-            Information
-                    </NavLink>
-                </li>
-                <li className={styles["flex-item"]}>
-                    <NavLink to="/home/skills-form"
-                        isActive={(match, location) => {              
-                            return location.pathname === "/home/skills-form";
-                        }
-                        }
-                        activeStyle={{
-                            fontWeight: "bold",
-                            color: "#047cc0"
-                        }}>
-            Skills
-                    </NavLink>
-                </li>
-                <li className={styles["flex-item"]}>
-                    <NavLink to="/home/portfolio-form"
-                        isActive={(match, location) => {              
-                            return location.pathname === "/home/portfolio-form";
-                        }
-                        }
-                        activeStyle={{
-                            fontWeight: "bold",
-                            color: "#047cc0"
-                        }}>
-            Portfolio
-                    </NavLink>
-                </li>
-            </ul>
-        </div>
+    const handleChangeLang=(e)=>{
+        props.i18n.changeLanguage(e.target.value);        
+    };
+   
+    return (
+        <I18n>
+            {(t, { i18n }) => (
+                <div className={styles.menu}>
+                    <div>
+                        <img src={IBM} alt="IBM" />
+                    </div>
+                    <ul className={styles["flex-container"]}>
+                        <li className={styles["flex-item"]}>
+                            <NavLink
+                                exact
+                                to="/home/info-form"
+                                isActive={(match, location) => {
+                                    return (
+                                        location.pathname === "/home" ||
+                    location.pathname === "/home/info-form"
+                                    );
+                                }}
+                                activeStyle={{
+                                    fontWeight: "bold",
+                                    color: "#047cc0"
+                                }}
+                            >
+                                {t("Information")}   
+                            </NavLink>
+                        </li>
+                        <li className={styles["flex-item"]}>
+                            <NavLink
+                                to="/home/skills-form"
+                                isActive={(match, location) => {
+                                    return location.pathname === "/home/skills-form";
+                                }}
+                                activeStyle={{
+                                    fontWeight: "bold",
+                                    color: "#047cc0"
+                                }}
+                            >
+                                {t("Skills")}
+                            </NavLink>
+                        </li>
+                        <li className={styles["flex-item"]}>
+                            <NavLink
+                                to="/home/portfolio-form"
+                                isActive={(match, location) => {
+                                    return location.pathname === "/home/portfolio-form";
+                                }}
+                                activeStyle={{
+                                    fontWeight: "bold",
+                                    color: "#047cc0"
+                                }}
+                            >
+                                {t("Portfolio")}
+                            </NavLink>
+                        </li>
+                        <select
+                            onChange={e=>handleChangeLang(e)}
+                            className={styles["flex-item"]}>                           
+                            <option value="en">English</option>
+                            <option value="fi">Finnish</option>
+                        </select>
+                    </ul>
+                </div>
+            )}
+        </I18n>
     );
 };
 
-export default withRouter(Header);
+export default translate()(withRouter(Header));
--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {createStore} from "redux";
 import {Provider} from "react-redux";
+import { I18nextProvider } from "react-i18next";
 import reducer from "./redux/reducers";
 import "./index.css";
 import App from "./components/App";
+import i18n from "./i18n";
 import registerServiceWorker from "./registerServiceWorker";
 
-let store = createStore(
-    reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+let store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
   
 ReactDOM.render(
     <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById("root"));
+        <I18nextProvider i18n={ i18n }><App />
+        </I18nextProvider></Provider>, document.getElementById("root"));
 registerServiceWorker();
